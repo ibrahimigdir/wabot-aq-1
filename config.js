4d77638a955f94b78924662b63c46053509b7b8b
ibrahimigdir <59979086+ibrahimigdir@users.noreply.github.com>
2022-03-02 13:42:37 +0300

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ runtime = process.uptime()
 teks = `${kyun(runtime)}`
 run = `${kyun(runtime)}`
 
-global.owner = ['601161739141']// Put your number here
+global.owner = ['13312095399']// Put your number here
 
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
